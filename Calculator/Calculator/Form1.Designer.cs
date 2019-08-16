7552abc9866e502251ec539fd5a7904d799a30c6
Seraphim Nombre <47295521+inuphim@users.noreply.github.com>
2019-08-16 12:27:41 +0800

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.TextBox();
+            this.btn_equal = new System.Windows.Forms.Button();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.btn_mult = new System.Windows.Forms.Button();
+            this.btn_divide = new System.Windows.Forms.Button();
+            this.btn_backspace = new System.Windows.Forms.Button();
+            this.btn_mod = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_dot = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.output.Enabled = false;
+            this.output.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.output.Location = new System.Drawing.Point(-4, -5);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(319, 97);
+            this.output.TabIndex = 0;
+            this.output.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_equal
+            // 
+            this.btn_equal.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_equal.Location = new System.Drawing.Point(251, 204);
+            this.btn_equal.Name = "btn_equal";
+            this.btn_equal.Size = new System.Drawing.Size(64, 168);
+            this.btn_equal.TabIndex = 1;
+            this.btn_equal.Text = "=";
+            this.btn_equal.UseVisualStyleBackColor = true;
+            this.btn_equal.Click += new System.EventHandler(this.Btn_equal_Click);
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(204, 316);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(48, 56);
+            this.btn_plus.TabIndex = 2;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.Btn_plus_Click);
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_minus.Location = new System.Drawing.Point(204, 260);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(48, 56);
+            this.btn_minus.TabIndex = 3;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.Click += new System.EventHandler(this.Btn_minus_Click);
+            // 
+            // btn_mult
+            // 
+            this.btn_mult.Location = new System.Drawing.Point(204, 204);
+            this.btn_mult.Name = "btn_mult";
+            this.btn_mult.Size = new System.Drawing.Size(48, 56);
+            this.btn_mult.TabIndex = 4;
+            this.btn_mult.Text = "*";
+            this.btn_mult.UseVisualStyleBackColor = true;
+            this.btn_mult.Click += new System.EventHandler(this.Btn_mult_Click);
+            // 
+            // btn_divide
+            // 
+            this.btn_divide.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_divide.Location = new System.Drawing.Point(204, 148);
+            this.btn_divide.Name = "btn_divide";
+            this.btn_divide.Size = new System.Drawing.Size(48, 56);
+            this.btn_divide.TabIndex = 5;
+            this.btn_divide.Text = "/";
+            this.btn_divide.UseVisualStyleBackColor = true;
+            this.btn_divide.Click += new System.EventHandler(this.Btn_divide_Click);
+            // 
+            // btn_backspace
+            // 
+            this.btn_backspace.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_backspace.Location = new System.Drawing.Point(204, 92);
+            this.btn_backspace.Name = "btn_backspace";
+            this.btn_backspace.Size = new System.Drawing.Size(48, 56);
+            this.btn_backspace.TabIndex = 6;
+            this.btn_backspace.Text = "←";
+            this.btn_backspace.UseVisualStyleBackColor = true;
+            this.btn_backspace.Click += new System.EventHandler(this.Btn_backspace_Click);
+            // 
+            // btn_mod
+            // 
+            this.btn_mod.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mod.Location = new System.Drawing.Point(252, 148);
+            this.btn_mod.Name = "btn_mod";
+            this.btn_mod.Size = new System.Drawing.Size(63, 56);
+            this.btn_mod.TabIndex = 7;
+            this.btn_mod.Text = "%";
+            this.btn_mod.UseVisualStyleBackColor = true;
+            this.btn_mod.Click += new System.EventHandler(this.Btn_mod_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(252, 92);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(63, 56);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.Btn_clear_Click);
+            // 
+            // btn_dot
+            // 
+            this.btn_dot.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dot.Location = new System.Drawing.Point(137, 316);
+            this.btn_dot.Name = "btn_dot";
+            this.btn_dot.Size = new System.Drawing.Size(67, 56);
+            this.btn_dot.TabIndex = 9;
+            this.btn_dot.Text = ".";
+            this.btn_dot.UseVisualStyleBackColor = true;
+            this.btn_dot.Click += new System.EventHandler(this.Btn_dot_Click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0.Location = new System.Drawing.Point(1, 316);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(136, 56);
+            this.btn_0.TabIndex = 10;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.Btn_0_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(1, 241);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(68, 75);
+            this.btn_1.TabIndex = 11;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.Btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.Location = new System.Drawing.Point(69, 241);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(68, 75);
+            this.btn_2.TabIndex = 12;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.Btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.Location = new System.Drawing.Point(137, 241);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(68, 75);
+            this.btn_3.TabIndex = 13;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.Btn_3_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_6.Location = new System.Drawing.Point(137, 167);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(68, 75);
+            this.btn_6.TabIndex = 16;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.Btn_6_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_5.Location = new System.Drawing.Point(69, 167);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(68, 75);
+            this.btn_5.TabIndex = 15;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.Btn_5_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.Location = new System.Drawing.Point(1, 167);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(68, 75);
+            this.btn_4.TabIndex = 14;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.Btn_4_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_9.Location = new System.Drawing.Point(137, 92);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(68, 75);
+            this.btn_9.TabIndex = 19;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.Btn_9_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_8.Location = new System.Drawing.Point(69, 92);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(68, 75);
+            this.btn_8.TabIndex = 18;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.Btn_8_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_7.Location = new System.Drawing.Point(1, 92);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(68, 75);
+            this.btn_7.TabIndex = 17;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.Btn_7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(312, 370);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_dot);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_mod);
+            this.Controls.Add(this.btn_backspace);
+            this.Controls.Add(this.btn_divide);
+            this.Controls.Add(this.btn_mult);
+            this.Controls.Add(this.btn_minus);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.btn_equal);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Calculator// by Seraphim Nombre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button btn_equal;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.Button btn_mult;
+        private System.Windows.Forms.Button btn_divide;
+        private System.Windows.Forms.Button btn_backspace;
+        private System.Windows.Forms.Button btn_mod;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_dot;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double fnum, snum,res;
+        String ops;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_0_Click(object sender, EventArgs e)
+        {
+            String input = output.Text+btn_0.Text;
+            output.Text = input;
+
+        }
+
+        private void Btn_1_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_1.Text;
+            output.Text = input;
+        }
+
+        private void Btn_2_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_2.Text;
+            output.Text = input;
+        }
+
+        private void Btn_3_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_3.Text;
+            output.Text = input;
+        }
+
+        private void Btn_4_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_4.Text;
+            output.Text = input;
+        }
+
+        private void Btn_5_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_5.Text;
+            output.Text = input;
+        }
+
+        private void Btn_6_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_6.Text;
+            output.Text = input;
+        }
+
+        private void Btn_7_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_7.Text;
+            output.Text = input;
+        }
+
+        private void Btn_plus_Click(object sender, EventArgs e)
+        {
+            ops = "+";
+            fnum = Convert.ToDouble(output.Text);
+            output.Text = "";
+        }
+
+        private void Btn_minus_Click(object sender, EventArgs e)
+        {
+            ops = "-";
+            fnum = Convert.ToDouble(output.Text);
+            output.Text = "";
+        }
+
+        private void Btn_8_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_8.Text;
+            output.Text = input;
+        }
+
+        private void Btn_mult_Click(object sender, EventArgs e)
+        {
+            ops = "*";
+            fnum = Convert.ToDouble(output.Text);
+            output.Text = "";
+        }
+
+        private void Btn_divide_Click(object sender, EventArgs e)
+        {
+            ops = "/";
+            fnum = Convert.ToDouble(output.Text);
+            output.Text = "";
+        }
+
+        private void Btn_mod_Click(object sender, EventArgs e)
+        {
+            ops = "%";
+            fnum = Convert.ToDouble(output.Text);
+            output.Text = "";
+        }
+
+        private void Btn_equal_Click(object sender, EventArgs e)
+        {
+            if (ops == "+")
+            {
+                snum = Convert.ToDouble(output.Text);
+                res = fnum + snum;
+                String result = res.ToString();
+                output.Text = result;
+            }else if(ops == "-")
+            {
+                snum = Convert.ToDouble(output.Text);
+                res = fnum - snum;
+                String result = res.ToString();
+                output.Text = result;
+            }else if(ops == "*")
+            {
+                snum = Convert.ToDouble(output.Text);
+                res = fnum * snum;
+                String result = res.ToString();
+                output.Text = result;
+            }else if(ops == "/")
+            {
+                snum = Convert.ToDouble(output.Text);
+                res = fnum / snum;
+                String result = res.ToString();
+                output.Text = result;
+            }else if(ops == "%")
+            {
+                snum = Convert.ToDouble(output.Text);
+                res = fnum % snum;
+                String result = res.ToString();
+                output.Text = result;
+            }
+        }
+
+        private void Btn_backspace_Click(object sender, EventArgs e)
+        {
+            if ((String.Compare(output.Text, " ") < 0))
+            {
+                output.Text = output.Text.Substring(0, output.Text.Length - 1 + 1);
+            }
+            else
+            {
+                output.Text = output.Text.Substring(0, output.Text.Length - 1);
+            }
+        }
+
+        private void Btn_clear_Click(object sender, EventArgs e)
+        {
+            output.Text = "";
+        }
+
+        private void Btn_9_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_9.Text;
+            output.Text = input;
+        }
+
+        private void Btn_dot_Click(object sender, EventArgs e)
+        {
+            String input = output.Text + btn_dot.Text;
+            output.Text = input;
+        }
+    }
+}
